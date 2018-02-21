d6a34924b5c657d8135e39f855be6cf4df46d142
EffeDiWeb <effedideveloper@gmail.com>
2018-02-21 12:47:23 +0100

    autorizzazioni

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221093257) do
+ActiveRecord::Schema.define(version: 20180221111147) do
 
   create_table "contents", force: :cascade do |t|
     t.string "titolo"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180221093257) do
     t.decimal "prezzo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
