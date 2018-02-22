166e7d6a3ecc3461c9907b9fac2ba2b65b76dc6e
EffeDiWeb <effedideveloper@gmail.com>
2018-02-22 15:25:00 +0100

    validazioni

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :contents
+
+  validates :name, presence: true
+  
 end
